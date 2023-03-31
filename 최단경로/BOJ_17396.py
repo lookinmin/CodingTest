121a3c688ffbc008585bbfac1fef18378ee2a574
Jo MINSU <50093223+lookinmin@users.noreply.github.com>
2023-03-31 11:02:55 +0900

    03.31 Day84 - 다익스트라, DP
    BOJ 9656 - S4
    # 조건확인 -> 홀/짝 확인 -> 구현
    BOJ 17396 - G5
    # 방문가능 조건이 추가된 다익스트라
    # if문에 조건 하나만 추가하면 가능

---
+++ 최단경로/BOJ_17396.py
@@ -0,0 +1,42 @@
+# 백도어, G5, 다익스트라
+
+from sys import stdin
+import heapq
+INF = int(1e9)
+n, m = map(int,stdin.readline().split())
+views = list(map(int,stdin.readline().split()))
+views[-1] = 0
+
+graph =[[]for _ in range(n+1)]
+dist = [INF] * (n+1)
+
+
+
+for _ in range(m):
+    a,b,c = map(int, stdin.readline().split())
+    graph[a].append((b,c))
+    graph[b].append((a,c))
+
+
+def dijkstra(s):
+    q = []
+    heapq.heappush(q, (0, s))
+    dist[s] = 0
+
+    while q:
+        distance,now = heapq.heappop(q)
+        if dist[now] < distance:
+            continue
+
+        for v, w in graph[now]:
+            cost = w + distance
+            if cost < dist[v] and views[v] == 0:        # 해당 넘버의 노드가 방문가능
+                dist[v] = cost
+                heapq.heappush(q, (cost, v))
+
+dijkstra(0)
+
+if dist[n-1] == INF:
+    print(-1)
+else:
+    print(dist[n-1])
