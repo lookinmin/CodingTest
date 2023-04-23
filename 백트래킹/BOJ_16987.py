fa9486ed17f160eaa94b80adf2d391c4adb020db
Jo MINSU <50093223+lookinmin@users.noreply.github.com>
2023-04-23 23:26:37 +0900

    04.23 Day104 - 완탐
    BOJ 16987 - G5(retry)
    # 감 잡기 힘들었음
    # 여러 상황들을 생각해서 구현해야 함
    # 잘 모르겠음 완탐식 구현은
    # 재귀가 어려움

---
+++ 백트래킹/BOJ_16987.py
@@ -0,0 +1,55 @@
+# 계란으로 계란치기, G5, 완탐-백트래킹
+# 계란으로 계란을 치게 되면 각 계란의 내구도는 상대 계란의 무게만큼 깎이게 된다
+from sys import stdin
+
+n = int(input())
+eggs = []
+answer = 0
+S, W = 0, 1
+
+for _ in range(n):
+    eggs.append(list(map(int, input().split())))
+
+
+def crash(nowIndex):
+    global answer
+    # 종료조건
+    if nowIndex == n:
+        breakEggs = 0
+        for i in range(n):
+            if eggs[i][S] <= 0:
+                breakEggs += 1
+        answer = max(answer, breakEggs)
+        return
+
+    # 자기가 깨져있는 경우 다음 계란으로
+    if eggs[nowIndex][S] <= 0:
+        crash(nowIndex + 1)
+        return
+
+    # 자기말고 다 깨져있는 상황인 경우
+    isAllBroken = True
+    for targetIndex in range(n):
+        if targetIndex == nowIndex: continue
+        if eggs[targetIndex][S] > 0:
+            isAllBroken = False
+            break
+    if isAllBroken:
+        answer = max(answer, n - 1)
+        return
+
+    # 때려보기
+    for targetIndex in range(n):
+        if targetIndex == nowIndex: continue
+        if eggs[targetIndex][S] <= 0: continue
+        # 때리기
+        eggs[nowIndex][S] -= eggs[targetIndex][W]
+        eggs[targetIndex][S] -= eggs[nowIndex][W]
+        crash(nowIndex + 1)
+        # 복구
+        eggs[nowIndex][S] += eggs[targetIndex][W]
+        eggs[targetIndex][S] += eggs[nowIndex][W]
+
+
+crash(0)
+print(answer)
