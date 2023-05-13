9fe088b9090d6d01f770b2602effc794f5950837
Jo MINSU <50093223+lookinmin@users.noreply.github.com>
2023-05-13 20:21:43 +0900

    Update BOJ_9465.py

--- DP/BOJ_9465.py
+++ DP/BOJ_9465.py
@@ -5,19 +5,21 @@ from sys import stdin
 T = int(stdin.readline())
 
 for _ in range(T):
-    n = int(stdin.readline().rstrip())
+    n = int(stdin.readline())
     board = []
     for _ in range(2):
         board.append(list(map(int, stdin.readline().split())))
 
-    board[0][1] += board[1][0]
-    board[1][1] += board[0][0]
 
-    for i in range(2, n):
-        board[0][i] += max(board[1][i-1], board[1][i-2])        # 대각선 바로 이전꺼, 혹은 그전꺼
-        board[1][i] += max(board[0][i-1], board[0][i-2])
 
-    res = max(board[0][n-1], board[1][n-1])
-    print(res)
+    for i in range(1, n):
+        if i == 1:
+            board[0][i] += board[1][i-1]
+            board[1][i] += board[0][i-1]
+        else:
+            board[0][i] += max(board[1][i-1], board[1][i-2])        # 대각선 바로 이전꺼, 혹은 그전꺼
+            board[1][i] += max(board[0][i-1], board[0][i-2])
+
+    print(max(board[0][n-1], board[1][n-1]))
 
 
