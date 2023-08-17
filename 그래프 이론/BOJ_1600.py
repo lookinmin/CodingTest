c9df5211e231571c04acb7cd059c5b28a5b9e265
Jo MINSU <50093223+lookinmin@users.noreply.github.com>
2023-08-17 17:41:13 +0900

    08.17 Day196 - BFS
    BOJ 1600 - G3(retry)
    # 횟수가 정해진 이동은 3차원으로
    # 굳이 cnt를 안쓰고 visited를 더하면서 가도 될 거 같음
    # 변경하고 다시 올릴것

---
+++ 그래프 이론/BOJ_1600.py
@@ -0,0 +1,47 @@
+# 말이 되고픈 원숭이, G3, BFS
+
+from sys import stdin
+from collections import deque
+
+# k번만 나이트 처럼 이동 가능
+k = int(stdin.readline())
+w, h = map(int, stdin.readline().split())
+graph = [[] for _ in range(h)]
+
+
+for i in range(h):
+    graph[i] = list(map(int, stdin.readline().split()))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, 1, -1]
+kx = [-2, -1, 1, 2, 2, 1, -1, -2]
+ky = [1, 2, 2, 1, -1, -2, -2, -1]
+
+def bfs(a, b, k):
+    visited = [[[0] * (k+1) for _ in range(w)] for _ in range(h)]
+    q = deque()
+    q.append((a,b,k,0))     # k만큼 움직일 수 있는 횟수, 이동횟수
+    visited[a][b][k] = 1
+
+    while q:
+        x, y, k, cnt = q.popleft()
+
+        if x == h-1 and y == w - 1:
+            return cnt
+        if k > 0:
+            for i in range(8):
+                nx = x + kx[i]
+                ny = y + ky[i]
+
+                if 0<=nx<h and 0<=ny<w and graph[nx][ny] == 0 and visited[nx][ny][k-1] == 0:
+                    visited[nx][ny][k-1] = 1
+                    q.append((nx,ny,k-1,cnt + 1))
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0<=nx<h and 0<=ny<w and graph[nx][ny] == 0 and visited[nx][ny][k] == 0:
+                visited[nx][ny][k] = 1
+                q.append((nx,ny,k,cnt+1))
+    return -1
+
+print(bfs(0,0, k))
