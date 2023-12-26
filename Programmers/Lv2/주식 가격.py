d8aa5fe3268e39bd2401ba5c0aab2f1c3abcebc3
Jo MINSU <50093223+lookinmin@users.noreply.github.com>
2023-12-26 21:53:38 +0900

    queue -> 이전 코드

--- Programmers/Lv2/주식 가격.py
+++ Programmers/Lv2/주식 가격.py
@@ -1,18 +1,13 @@
-from collections import deque
-
-
 def solution(prices):
     answer = []
-    q = deque(prices)
-
-    while q:
-        target = q.popleft()  # 기준
+    for idx in range(len(prices) - 1):
+        # stack = []
         cnt = 0
-
-        for num in q:
+        tmp = prices[idx]       # 현재 값 (기준)
+        for i in range(idx + 1, len(prices)):
             cnt += 1
-            if target > num:  # 더 크면 끝
+            if prices[i] < tmp:
                 break
         answer.append(cnt)
-
+    answer.append(0)
     return answer
