fb9d3a6dee4e8149ced37f78af3cd0cc24543fe3
Jo MINSU <50093223+lookinmin@users.noreply.github.com>
2024-01-27 22:55:10 +0900

    heapq 풀이 방법 추가
    원래 하려던게 저런 느낌이었나

--- Programmers/Lv3/숫자 게임.py
+++ Programmers/Lv3/숫자 게임.py
@@ -13,3 +13,24 @@ def solution(A, B):
             answer += 1
             del B[0]
     return answer
+
+import heapq
+def sol(a, b):
+    a = [-i for i in a]
+    b = [-i for i in b]
+    # 최대힙을 위해 그냥 원소를 음수로 바꿔버림
+
+    heapq.heapify(a)
+    heapq.heapify(b)
+
+    answer = 0
+
+    while b and a:
+        num_a = heapq.heappop(a)
+        num_b = heapq.heappop(b)
+
+        if -num_a < -num_b:
+            answer += 1
+        else:
+            heapq.heappush(b, num_b)
+    return answer
