9ca7f502c90f7c5c10a4517c82252493ced86992
lookinmin <ancx1234@naver.com>
2024-09-25 23:54:08 +0900

    9.25 Day483

---
+++ 구현/BOJ_21314.py
@@ -0,0 +1,40 @@
+# S1, 민겸 수
+minkum = input().rstrip()
+minValue = ""
+maxValue = ""
+
+cnt = 0
+for w in minkum:
+    if w == 'M':
+        cnt += 1
+    else:
+        if cnt == 0:
+            minValue += '5'
+            maxValue += '5'
+            continue
+        else:
+            # 앞에 n개의 M이 있고 K로 끝남
+            
+            maxtmp = 5 * (10**cnt)
+            mintmp = 10**(cnt - 1)
+
+            minValue += str(mintmp)
+            minValue += '5'
+            
+            maxValue += str(maxtmp)
+        
+            cnt = 0
+
+if cnt > 0:
+    if minkum[-1] == 'K':
+        maxtmp = 5 * (10**(cnt))
+        maxValue += str(maxtmp)
+    else:
+        maxtmp = '1' * cnt
+        mintmp = 10**(cnt - 1)
+
+        maxValue += str(maxtmp)
+        minValue += str(mintmp)
+
+print(maxValue)
+print(minValue)
