18231d659467ae8bcc1c610c3db52886fab45250
lookinmin <ancx1234@naver.com>
2025-06-24 23:48:43 +0900

    Day 570 - Refactoring

--- Programmers/Lv1/[PCCP 기출문제] 1번_동영상 재생기.py
+++ Programmers/Lv1/[PCCP 기출문제] 1번_동영상 재생기.py
@@ -1,38 +1,34 @@
+def time_to_seconds(time_str):
+    minutes, seconds = map(int, time_str.split(':'))
+    return minutes * 60 + seconds
+
+def seconds_to_time(seconds):
+    return "{:02d}:{:02d}".format(seconds // 60, seconds % 60)
+
+
 def solution(video_len, pos, op_start, op_end, commands):
     # 동영상 길이, 직전 재생위치, 오프닝 시작, 끝, 사용자 입력
     # 사용자 입력 끝난 후의 동영상 위치 리턴
     
     # next : +10s
     # prev : -10s
-    end = int(video_len.split(':')[0]) * 60 + int(video_len.split(':')[1])
-    op_st = int(op_start.split(':')[0]) * 60 + int(op_start.split(':')[1])
-    op_ed = int(op_end.split(':')[0]) * 60 + int(op_end.split(':')[1])
+    video_end = time_to_seconds(video_len)
+    op_st = time_to_seconds(op_start)
+    op_ed = time_to_seconds(op_end)
     
-    now_min = int(pos.split(':')[0])
-    now_sec = int(pos.split(':')[1])
-    
-    now = now_min * 60 + now_sec
-    for com in commands:
+    now = time_to_seconds(pos)
+    for command in commands:
         if op_st <= now <= op_ed:
             now = op_ed
         
-        if com == 'prev':
+        if command == 'prev':
             # -10
-            if now - 10 >= 0:
-                now -= 10
-            else:
-                now = 0
+            now = max(0, now - 10)
         else:
             # +10
-            if now + 10 <= end:
-                now += 10
-            else:
-                now = end
+            now = min(video_end, now + 10)
     
     if op_st <= now <= op_ed:
         now = op_ed
     
-    ans_min = now // 60
-    ans_sec = now % 60
-    answer = "{:02d}:{:02d}".format(ans_min, ans_sec)
-    return answer
+    return seconds_to_time(now)
